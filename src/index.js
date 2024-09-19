0612f5d70a33aadaa648da00b195fe24a4f2d125
Al Cerdan Lico <alcerdan@yahoo.com>
2024-09-19 12:54:14 -0700

    completed tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target["new-task-description"].value)
+    form.reset();
+  })
+})
+
+function buildToDo(todo) {
+  let li = document.createElement("li");
+  li.textContent = `${todo}  `;
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x';
+  li.appendChild(btn);
+  console.log(li);
+  document.getElementById('tasks').appendChild(li)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
